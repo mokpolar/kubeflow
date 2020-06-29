5e42d85c488a8a4b4cbe62cc31ae824d1bb3424d
Jung Ju Young <beholder16@gmail.com>
2020-06-29 14:57:51 +0900

    20200628_1

--- eks/samples/mobilenet_tf_kfserving/transformer/simple_mobilenet_transformer.py
+++ eks/samples/mobilenet_tf_kfserving/transformer/simple_mobilenet_transformer.py
@@ -16,9 +16,6 @@ from PIL import Image
 
 import time
 
-start = time.time()
-
-
 # for MobileNet Prediction Parsing
 from tensorflow.keras.applications.mobilenet import decode_predictions
 import tensorflow as tf
@@ -32,11 +29,12 @@ logging.basicConfig(level=kfserving.constants.KFSERVING_LOGLEVEL)
 def image_transform(instance):
     img = base64.b64decode(instance['image']['b64'])
     img = Image.open(io.BytesIO(img))
+    logging.info("img loadind complete", time.time())
     img = img.resize((224, 224))
     img = np.array(img)
     img = img/255
     img = img.reshape(-1, 224, 224, 3)
-    logging.info(img)
+    logging.info("img reshaping complete", time.time())
     return img.tolist()
 
 # postprocess will parse the prediction and get class name, accuracy
@@ -54,11 +52,13 @@ class ImageTransformer(kfserving.KFModel):
         self.predictor_host = predictor_host
 
     def preprocess(self, inputs: Dict) -> Dict:
+        logging.info("start preprocessing", time.time()) # when input arrives
         return {'instances': image_transform(inputs['instances'][0])}
 
     def postprocess(self, inputs: Dict) -> Dict:
-        end = time.time()
-        return {'predictions': [parsing_prediction(prediction) for prediction in inputs['predictions']], 'time' : [end - start]}
+        start_post = time.time()
+        logging.info("start postprocessing", time.time()) # when prediction arrvies
+        return {'predictions': [parsing_prediction(prediction) for prediction in inputs['predictions']]}
 
 
 if __name__ == "__main__":
